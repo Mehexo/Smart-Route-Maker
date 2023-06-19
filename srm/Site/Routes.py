5dfa534ad59d52e52a72e2e26f661dfdab1e6a9e
Mervin Hanssen <64969624+Mehexo@users.noreply.github.com>
2023-06-19 12:34:55 +0200

    Delete srm directory

--- srm/Site/Routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-site = Blueprint('Site', __name__,
-    static_folder='Static',
-    template_folder='Templates')
-
-@site.route('/site')
-def siteindex():
-    return 'Placeholder site page.'
